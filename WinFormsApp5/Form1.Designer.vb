a09c8690c4c73dbacfae4885f341c153b47e58e6
mostafa20021212 <alskjdjskssk@gmail.com>
2025-01-15 10:10:07 +0200

    Initial commit

---
+++ WinFormsApp5/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        CheckBox1 = New CheckBox()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackgroundImage = My.Resources.Resources._6921087_ai
+        resources.ApplyResources(Button1, "Button1")
+        Button1.ForeColor = Color.Brown
+        Button1.Name = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        resources.ApplyResources(CheckBox1, "CheckBox1")
+        CheckBox1.BackColor = Color.Transparent
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        resources.ApplyResources(Label1, "Label1")
+        Label1.ForeColor = Color.IndianRed
+        Label1.Name = "Label1"
+        ' 
+        ' TextBox1
+        ' 
+        resources.ApplyResources(TextBox1, "TextBox1")
+        TextBox1.Name = "TextBox1"
+        ' 
+        ' TextBox2
+        ' 
+        resources.ApplyResources(TextBox2, "TextBox2")
+        TextBox2.Name = "TextBox2"
+        ' 
+        ' Form1
+        ' 
+        resources.ApplyResources(Me, "$this")
+        AutoScaleMode = AutoScaleMode.Font
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button1)
+        DoubleBuffered = True
+        Name = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
